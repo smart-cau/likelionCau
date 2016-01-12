c385425312bee53b7ab18208bb3b1910d83b2fc9
Yong Jae Kim <yongjaekim@Yongui-MacBook-Pro.local>
2016-01-12 13:19:09 +0900

    routes.rb 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  match ":controller(/:action(/:id))", :via => [:post, :get]
+  root :to => 'likelion#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
